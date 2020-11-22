2f8a130b543fcdef53a124bdf01eeac45ed2571b
enotdgk <enotdgk@mail.com>
2020-11-22 13:31:20 +0300

    update

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,12 @@
+let accordions = document.querySelectorAll('.accordion')
+
+for (let i = 0; i < accordions.length; i++) {
+    accordions[i].addEventListener('click', function () {
+        let hideTextBlock = accordions[i].nextElementSibling
+        hideTextBlock.classList.toggle('active_text')
+        !hideTextBlock.classList.toggle('hide_text')
+    })
+}
 
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIlxyXG4iXSwiZmlsZSI6Im1haW4uanMifQ==
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImxldCBhY2NvcmRpb25zID0gZG9jdW1lbnQucXVlcnlTZWxlY3RvckFsbCgnLmFjY29yZGlvbicpXHJcblxyXG5mb3IgKGxldCBpID0gMDsgaSA8IGFjY29yZGlvbnMubGVuZ3RoOyBpKyspIHtcclxuICAgIGFjY29yZGlvbnNbaV0uYWRkRXZlbnRMaXN0ZW5lcignY2xpY2snLCBmdW5jdGlvbiAoKSB7XHJcbiAgICAgICAgbGV0IGhpZGVUZXh0QmxvY2sgPSBhY2NvcmRpb25zW2ldLm5leHRFbGVtZW50U2libGluZ1xyXG4gICAgICAgIGhpZGVUZXh0QmxvY2suY2xhc3NMaXN0LnRvZ2dsZSgnYWN0aXZlX3RleHQnKVxyXG4gICAgICAgICFoaWRlVGV4dEJsb2NrLmNsYXNzTGlzdC50b2dnbGUoJ2hpZGVfdGV4dCcpXHJcbiAgICB9KVxyXG59XHJcblxyXG4iXSwiZmlsZSI6Im1haW4uanMifQ==
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
+let accordions = document.querySelectorAll('.accordion')
+
+for (let i = 0; i < accordions.length; i++) {
+    accordions[i].addEventListener('click', function () {
+        let hideTextBlock = accordions[i].nextElementSibling
+        hideTextBlock.classList.toggle('active_text')
+        !hideTextBlock.classList.toggle('hide_text')
+    })
+}
 
